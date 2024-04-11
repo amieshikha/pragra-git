cc5058a4a8bd353a3737d982e12f99d356f549bd
Shikha <shikha.sharma24mca@gmail.com>
2024-04-11 18:01:22 -0400

    MO

---
+++ src/Assignments/Assignment4/MOShape.java
@@ -0,0 +1,16 @@
+package Assignments.Assignment4;
+
+public class MOShape {
+
+    public double area(double radius){
+        return Math.PI * radius * radius;
+    }
+    public double area(double length, double width){
+        return length * width;
+    }
+    public double area(int side)
+    {
+        return side * side;
+    }
+
+}
--- src/Assignments/Assignment4/ShapeMain.java
+++ src/Assignments/Assignment4/ShapeMain.java
@@ -2,11 +2,14 @@ package Assignments.Assignment4;
 
 public class ShapeMain {
     public static void main(String[] args) {
-        Shape shape= new Shape(4,2, 1, 2);
-        shape.areaOfCircle();
-        shape.areaOfSquare();
-        shape.areaOfRectangle();
-
+//        Shape shape= new Shape(4,2, 1, 2);
+//        shape.areaOfCircle();
+//        shape.areaOfSquare();
+//        shape.areaOfRectangle();
+MOShape mo= new MOShape();
+        System.out.println("Area of rectangle is " +mo.area(2, 4));
+        System.out.println("Area of circle is " +mo.area(Math.PI * 1.2 * 1.2));
+        System.out.println("Area of square is " +mo.area(2.2));
     }
 }
- // 50.24, 4.0, 2.0
+ // 50.26, 4.0, 2.0
